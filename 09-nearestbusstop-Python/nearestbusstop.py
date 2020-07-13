e0be3495ca3f92a6238f359ee0a6b2abd729b49a
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-13 20:26:56 +0530

    Jul 13  20:26:56

--- 09-nearestbusstop-Python/nearestbusstop.py
+++ 09-nearestbusstop-Python/nearestbusstop.py
@@ -1,11 +1,18 @@
-# Write the function nearestBusStop(street) that takes a 
-# non-negative int street number, and returns the nearest 
-# bus stop to the given street, where buses stop every 8th street, 
-# including street 0, and ties go to the lower street, 
-# so the nearest bus stop to 12th street is 8th street, 
+# Write the function nearestBusStop(street) that takes a
+# non-negative int street number, and returns the nearest
+# bus stop to the given street, where buses stop every 8th street,
+# including street 0, and ties go to the lower street,
+# so the nearest bus stop to 12th street is 8th street,
 # and the nearest bus stop to 13 street is 16th street.
 
 
 
 def fun_nearestbusstop(street):
-	return -1
+
+	c= street -(street%8)
+	c1 =(street+8) - (street%8)
+	if(abs(street-c) < abs(c1-street)):
+		return abs(street-c)
+	else:
+		return 	abs(c1-street)
+
