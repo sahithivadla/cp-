3c1f2b718932087c77131819689402183e8e8788
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-13 18:43:26 +0530

    Jul 13  18:43:26

--- 03-distance-Python/distance.py
+++ 03-distance-Python/distance.py
@@ -1,9 +1,11 @@
-# Write the function fun_distance(x1, y1, x2, y2) 
-# that takes four int values x1, y1, x2, y2 
-# that represent the two points (x1, y1) and (x2, y2), 
+# Write the function fun_distance(x1, y1, x2, y2)
+# that takes four int values x1, y1, x2, y2
+# that represent the two points (x1, y1) and (x2, y2),
 # and returns the distance between those points as a int.
 
 
 def fun_distance(x1, y1, x2, y2):
 	# your code goes here
-	return 0
+	m =0
+	m =(y2-y1)//(x2-x1)
+	return m
