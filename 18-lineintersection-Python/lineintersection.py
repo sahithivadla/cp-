56c8f730380ea6ea37e5886c4d84118b7fe4c512
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-13 22:40:05 +0530

    l

--- 18-lineintersection-Python/lineintersection.py
+++ 18-lineintersection-Python/lineintersection.py
@@ -5,13 +5,17 @@
 # This function returns the x value of the point of intersection of the two lines. If the lines are parallel, or identical, the function should return None.
 
 import numpy as np
-
+import math
 def lineintersection(m1, b1, m2, b2):
 	# your code goes here
 
+	if(m1==m2 or m1%m2 ==0 or m2%m1==0):
+		return None
+	else:
 
-	A = np.array([[m1, 1], [m2, 1]])
-	B = np.array([[b1], [b2]])
-	print(np.linalg.inv(A) )
+		A = np.array([[m1, 1], [m2, 1]])
+		B = np.array([[b1], [b2]])
+		ans = np.linalg.inv(A) @B
+		return(abs(int(math.floor(ans[0]))))
 
 
