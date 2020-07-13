0d656e7077f64b17614499e1caef22895aad2a39
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-13 22:00:56 +0530

    Jul 13  22:00:56

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -7,4 +7,15 @@
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+
+	a = ((x1 - x2) * (x1 - x2)) + \
+        ((y1 - y2) * (y1 - y2))
+	b = ((x1 - x3) * (x1 - x3)) + \
+        ((y1 - y3) * (y1 - y3))
+	c = ((x2 - x3) * (x2 - x3)) + \
+        ((y2 - y3) * (y2 -y3))
+
+
+
+
+
