8792e1fbf9ce76463778500b0c276f6a296e032d
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-14 19:26:34 +0530

    Jul 14  19:26:34

--- 03-recursion-Python/recursion.py
+++ 03-recursion-Python/recursion.py
@@ -1,7 +1,18 @@
 """Implement a function recursively to get the desired
 Fibonacci sequence value.
-Your code should have the same input/output as the 
+Your code should have the same input/output as the
 iterative code in the instructions."""
 
 def get_fib(position):
+
+    if position ==0 :
+        return 0
+    elif position ==1:
+        return 1
+    else:
+        return get_fib(position-1)+get_fib(position-2)
+
+
+
+
     return -1
