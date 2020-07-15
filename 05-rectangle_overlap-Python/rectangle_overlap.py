c3a52e780bd72444215c26048ccba523f2a6666b
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-15 12:15:59 +0530

    Jul 15  12:15:59

--- 05-rectangle_overlap-Python/rectangle_overlap.py
+++ 05-rectangle_overlap-Python/rectangle_overlap.py
@@ -1,13 +1,17 @@
 # rectanglesOverlap(left1, top1, width1, height1, left2, top2, width2, height2)
-# A rectangle can be described by its left, top, width, and height. This function 
-# takes two rectangles described this way, and returns True if the rectangles 
-# overlap at all (even if just at a point), and False otherwise. 
-# Note: here we will represent coordinates the way they are usually represented in 
-# computer graphics, where (0,0) is at the left-top corner of the screen, and while 
-# the x-coordinate goes up while you head right, the y-coordinate goes up while you 
+# A rectangle can be described by its left, top, width, and height. This function
+# takes two rectangles described this way, and returns True if the rectangles
+# overlap at all (even if just at a point), and False otherwise.
+# Note: here we will represent coordinates the way they are usually represented in
+# computer graphics, where (0,0) is at the left-top corner of the screen, and while
+# the x-coordinate goes up while you head right, the y-coordinate goes up while you
 # head down (so we say that "up is down")
 
 def fun_rectangle_overlap(left1, top1, width1, height1, left2, top2, width2, height2):
-    return True
+    if(left1 >= width2 or left2 >= width1):
+        return False
 
-        
+    if(l1.y <= r2.y or l2.y <= r1.y):
+        return False
+
+    return True
