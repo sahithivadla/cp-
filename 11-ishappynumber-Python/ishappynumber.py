ff591ed7764ae37f86afb59c903129c8bc3997b7
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-15 15:26:52 +0530

    Jul 15  15:26:51

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -1,10 +1,10 @@
 # ishappynumber(n) [10 pts]
-# A happy number is a number defined by the following process: Starting with any positive integer, replace the 
-# number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will 
-# stay ), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 
+# A happy number is a number defined by the following process: Starting with any positive integer, replace the
+# number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will
+# stay ), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1
 # are happy numbers.
 
-# Write the function ishappynumber(n) which takes a possibly-negative integer and returns True if it is happy and 
+# Write the function ishappynumber(n) which takes a possibly-negative integer and returns True if it is happy and
 # False otherwise. Note that all numbers less than 1 are not happy. Here are some test assertions for you:
 # assert(ishappynumber(-7) == False)
 # assert(ishappynumber(1) == True)
@@ -16,4 +16,16 @@
 
 def ishappynumber(n):
 	# your code goes here
-	pass
+	if(n == 1):
+		return True
+	else:
+		while(len(n)!=1):
+			if len(str(n)) == 3 :
+				n = int(str(n)[0]) **2 + int(str(n)[1]) **2 + int(str(n)[2]) **2
+			elif  len(str(n)) == 2 :
+				n = int(str(n)[0]) **2 + int(str(n)[1]) **2
+		if(n==1):
+			return True
+		return False
+
+
