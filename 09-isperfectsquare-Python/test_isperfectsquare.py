ed420c9339b253e5923a797e420fb6186f602472
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-15 10:37:01 +0530

    Jul 15  10:37:00

--- 09-isperfectsquare-Python/test_isperfectsquare.py
+++ 09-isperfectsquare-Python/test_isperfectsquare.py
@@ -5,13 +5,13 @@ import pytest
 
 
 @pytest.mark.parametrize('n, result',[
-	("hello", False), 
-	(6.25, False), 
-	(625, True), 
-	(-625, False), 
-	(4, True), 
-	(100, True), 
-	("100", True), 
+	("hello", False),
+	(6.25, False),
+	(625, True),
+	(-625, False),
+	(4, True),
+	(100, True),
+	("100", True),
 ])
 def test_isperfectsquare(row, result):
     assert isperfectsquare(row) == result
