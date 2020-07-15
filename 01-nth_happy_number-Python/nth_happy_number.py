e406fa195f3c41d4a15cc6f464fae17f71c52d00
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-15 18:20:25 +0530

    Jul 15  18:20:24

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -1,5 +1,5 @@
-# Write the function nthHappyNumber(n) which takes a non-negative integer 
-# and returns the nth happy number (where the 0th happy number is 1). 
+# Write the function nthHappyNumber(n) which takes a non-negative integer
+# and returns the nth happy number (where the 0th happy number is 1).
 # Here are some test assertions for you:
 # assert(nthHappyNumber(0) == 1)
 # assert(nthHappyNumber(1) == 7)
@@ -9,7 +9,41 @@
 # assert(nthHappyNumber(5) == 23)
 # assert(nthHappyNumber(6) == 28)
 # assert(nthHappyNumber(7) == 31)
+def ishappynumber(n):
+	n =abs(n)
+	if(n == 1):
+		return True
+	elif(len(str(n))==1):
+		n=n*n
+		while(n!=1):
+			if len(str(n)) == 3 :
+				n = int(str(n)[0]) **2 + int(str(n)[1]) **2 + int(str(n)[2]) **2
+			elif  len(str(n)) == 2 :
+				n = int(str(n)[0]) **2 + int(str(n)[1]) **2
+
+	else:
+		while(len(str(n))!=1):
+			if len(str(n)) == 3 :
+				n = int(str(n)[0]) **2 + int(str(n)[1]) **2 + int(str(n)[2]) **2
+			elif  len(str(n)) == 2 :
+				n = int(str(n)[0]) **2 + int(str(n)[1]) **2
+
+		if(n==1):
+			return True
+		return False
+def alllist():
+	l=[]
+	for i in range(0,50):
+		if(ishappynumber(i) == True):
+			l.append(i)
+	return l
+
 
 
 def fun_nth_happy_number(n):
-	return 0
+	t =[]
+	t= alllist()
+
+	# return t[n]
+
+
