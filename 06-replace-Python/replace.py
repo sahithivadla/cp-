0209b3bcd6f1454b6f5445c8d1ab6c12d74af745
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-15 23:01:30 +0530

    Jul 15  23:01:29

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -5,15 +5,17 @@
 
 
 def fun_replace(s1, s2, s3):
-
-		x = [i for i in range(len(s1)) if s1.startswith(s2, i)]
+		x=[]
 		if(len(x)==0):
 			return s1
 		else:
-			res =s1
-			for ind in x:
-				res = res[0:ind]+s3+res[ind+len(s2):]
+			res=""
+			res=s1
+			while(len(x)!=0):
+				x = [i for i in range(len(res)) if res.startswith(s2,i)]
+				res = res[0:x[0]]+s3+res[x[0]+len(s2):]
 			return res
+
 
 
 
