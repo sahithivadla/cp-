2e5be888e85641eb818f2bd68ea0da446603bdff
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-17 18:25:25 +0530

    Jul 17  18:25:24

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -12,31 +12,51 @@ class Element(object):
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
 class LinkedList(object):
     def __init__(self, head=None):
         self.head = head
-        
+
     def append(self, new_element):
         # Your code goes here
-        pass
-            
+        cur = self.head
+        while(cur!=None):
+            cur = cur.next
+        cur.next = new_element
+
+
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
         # Your code goes here
-        pass
-    
+        cur = self.head
+        c= 0
+        while(cur!=None):
+            if(c==position):
+                return cur
+            cur =cur.next
+            c=c+1
+        return None
+
+
     def insert(self, new_element, position):
         """Insert a new node at the given position.
         Assume the first position is "1".
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
         # Your code goes here
-        pass
-    
-    
+        cur =self.head
+        c = 0
+        while(cur!=None):
+            if(c+1 == position):
+                rws =cur.next
+                cur.next = new_element
+                new_element.next  =rws
+
+
+
+
     def delete(self, value):
         """Delete the first node with a given value."""
         # Your code goes here
