f15b210212c3178dfd646517485af04e4cf1120a
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-17 19:11:31 +0530

    Jul 17  19:11:28

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -14,4 +14,14 @@ def binary_search(input_array, value):
     low = 0
     high = len(arr) - 1
     mid = 0
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+
+    return -1
 
