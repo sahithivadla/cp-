8e914f1577fd024194eed8fd6786b76695c5495b
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-18 18:56:19 +0530

    Jul 18  18:56:18

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -11,7 +11,7 @@ def isrotation(x, y):
 		return True
 	if(len(str(x))!= len(str(y))):
 		return False
-	if(str(x)[0]+str(x)[1]+str(x)[2] not in y):
+	if(str(x)[0]+str(x)[1]+str(x)[2] not in str(y)):
 		return False
 
 	pass
