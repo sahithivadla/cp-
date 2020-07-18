aabf2d72ad67f2e07300c412f2dc236424ae96d4
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-18 20:22:31 +0530

    Jul 18  20:22:30

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -10,12 +10,30 @@ class BST(object):
 
     def insert(self, new_val):
         # Your code goes here
+        if new_val < x.value :
+
+            if x.left == None :
+
+                x.left = Node(new_val)
+
+            else :
+
+                self._insert(new_val, x.left)
+        else :
+
+            if x.right == None :
+
+                x.right = Node(new_val)
+
+            else :
+
+                self._insert(new_val, self.right)
         pass
 
     def printSelf(self):
         # Your code goes here
         pass
-        
+
     def search(self, find_val):
         # Your code goes here
         pass
