ab42274ec583c9a449f31177ce1b314331386120
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-18 12:13:11 +0530

    Jul 18  12:13:10

--- 04-getaverage-Python/test_getaverage.py
+++ 04-getaverage-Python/test_getaverage.py
@@ -4,7 +4,7 @@ from getaverage import fun_getaverage
 import pytest
 
 
-@pytest.mark.parametrize('s1, result',[	
+@pytest.mark.parametrize('s1, result',[
 ("13,excused,14,absent", 13.5), ("a,b,c",0.0),
 ("a,12,c,14,6,0", 8.0), ("1,2,3,4,5,6,7,8,9,10", 5.5),
 ("2,3,ddd4ff,34,1", 10.0)
