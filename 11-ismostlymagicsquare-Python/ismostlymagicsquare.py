678a89087f42657a6ef0fcbf038cb3d4f389dfcb
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-18 14:21:43 +0530

    tt

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -1,9 +1,9 @@
 # isMostlyMagicSquare(a) [15 pts]
-# Write the function isMostlyMagicSquare(a) that takes an 2d list of numbers, which you may assume is an NxN square 
+# Write the function isMostlyMagicSquare(a) that takes an 2d list of numbers, which you may assume is an NxN square
 # with N>0, and returns True if it is "mostly magic" and False otherwise, where a square is "mostly magic" if:
 # Each row, each column, and each of the 2 diagonals each sum to the same total.
-# A completely magic square has additional restrictions (such as not allowing duplicates, and only allowing numbers 
-# from 1 to N2), which we are not enforcing here, but which you can read about here. Note: any magic square is also 
+# A completely magic square has additional restrictions (such as not allowing duplicates, and only allowing numbers
+# from 1 to N2), which we are not enforcing here, but which you can read about here. Note: any magic square is also
 # a "mostly magic" square, including this sample magic square:
 # Read for more: https://en.wikipedia.org/wiki/Magic_square
 # Here is another mostly-magic square:
@@ -15,4 +15,11 @@
 
 def ismostlymagicsquare(a):
 	# Your code goes here
-	pass
+	s = sum(a[0])
+	for i in range(1,len(a)):
+		if (sum(a[i])!= s):
+			return False
+	return True
+
+
+
