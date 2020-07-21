3fa5ff4e6244ef406f3db50e66987fc14de93830
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-21 13:58:04 +0530

    t

--- 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
+++ 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
@@ -9,16 +9,13 @@ from collections import Counter
 def fixmostlymagicsquare(L):
 	s = []
 	for i in range(0,len(L)):
-		for j in range(0,len(L[i])):
-			s.append(sum(l[i][j]))
+		s.append(sum(L[i]))
 	counter  = Counter(s)
-	s = s.index(min(counter,key=counter.get))
 	a = s.index(min(counter,key=counter.get))
 	c = sum(L[a]) - sum(L[a+1])
 	d =  L[a][-1]
 	L[a][-1] = d - c
 	return(L)
-
 
 
 	# Your code goes here
