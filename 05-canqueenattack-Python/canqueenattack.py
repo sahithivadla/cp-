c40bf0be2e9534a9b242d1946231ecffc254e09b
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-21 14:21:16 +0530

    t

--- 05-canqueenattack-Python/canqueenattack.py
+++ 05-canqueenattack-Python/canqueenattack.py
@@ -1,8 +1,12 @@
 # canQueenAttack(qX, qY, oX, oY) [15 pts]
-# Given the position of the queen (qX, qY) and the opponent (oX, oY) on a chessboard. The task is to determine 
-# whether the queen can attack the opponent or not. Note that the queen can attack in the same row, same column and 
+# Given the position of the queen (qX, qY) and the opponent (oX, oY) on a chessboard. The task is to determine
+# whether the queen can attack the opponent or not. Note that the queen can attack in the same row, same column and
 # diagonally.
 
 def canqueenattack(qR, qC, oR, oC):
 	# Your code goes here
+	if(qR == oR or oC == qC or abs(qR-oR) == abs(qC-oC)):
+		return True
+	return False
+
 	pass
