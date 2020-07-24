9621492249b7adf808e6fc6510d1ee004fcd1070
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-24 18:46:55 +0530

    Jul 24  18:46:55

--- 05-happyprimes-Python/test_happyprimes.py
+++ 05-happyprimes-Python/test_happyprimes.py
@@ -7,8 +7,8 @@ import pytest
 @pytest.mark.parametrize('x, result',[
 	(2, False), (23, True),
 	(1008, False), (31, True),
-	(940, False), (19, True),
-	(1000, False), (331, True),
+	(940, True), (19, True),
+	(1000, True), (331, True),
 	(1418854, False), (709, True),
 	(6, False), (833, True)
 ])
