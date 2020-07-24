b0ac72df5c6eeaba56646f5a7d306bdb8eba0b0e
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-24 22:23:39 +0530

    Jul 24  22:23:37

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -1,4 +1,4 @@
-# Write the function nthTidyNumber that takes a non-negative int n and returns the nth Tidy Number. 
+# Write the function nthTidyNumber that takes a non-negative int n and returns the nth Tidy Number.
 # A tidy number is a number whose digits are in non-decreasing order.
 # fun_nth_tidynumber(0) = 1
 # fun_nth_tidynumber(1) = 2
@@ -7,4 +7,5 @@
 # fun_nth_tidynumber(35) = 46
 
 def fun_nth_tidynumber(n):
+
     return 0
