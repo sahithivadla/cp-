f882678709bcf8c0872478b4063277411b689158
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-24 18:18:21 +0530

    Jul 24  18:18:21

--- 04-nth_carolprime-Python/nth_carolprime.py
+++ 04-nth_carolprime-Python/nth_carolprime.py
@@ -1,13 +1,34 @@
-# Write the function nthCarolPrime(n), which takes a non-negative int and returns the nth Carol Prime, 
-# which is a prime number of the form ((2**k - 1)**2 - 2) for some value positive int k. For example, 
-# if k equals 3, ((2**3 - 1)**2 -2) equals 47, which is prime, and so 47 is a Carol Prime. 
-# The first several Carol primes are: 7, 47, 223, 959, 3967, 16127, 65023, 261119, 1046527,... As such, 
+# Write the function nthCarolPrime(n), which takes a non-negative int and returns the nth Carol Prime,
+# which is a prime number of the form ((2**k - 1)**2 - 2) for some value positive int k. For example,
+# if k equals 3, ((2**3 - 1)**2 -2) equals 47, which is prime, and so 47 is a Carol Prime.
+# The first several Carol primes are: 7, 47, 223, 959, 3967, 16127, 65023, 261119, 1046527,... As such,
 # nthCarolPrime(0) returns 7.
-# Note: You must use a reasonably efficient approach that quickly works up to n==9, which 
-# will return a 12-digit answer! In particular, this means you cannot just edit isPrime. 
-# Hint: you may need to generate only Carol numbers, and then test those as you go 
+# Note: You must use a reasonably efficient approach that quickly works up to n==9, which
+# will return a 12-digit answer! In particular, this means you cannot just edit isPrime.
+# Hint: you may need to generate only Carol numbers, and then test those as you go
 # for primality (and you may need to think about that hint for a while for it to make sense!).
 
 
+def isprime(num):
+    c = 0
+    for i in range(1,num+1):
+        if(num%i == 0):
+            c = c+1
+    if(c == 2):
+        return  True
+    return False
+
+
 def fun_nth_carolprime(n):
+    start = 2
+    k = 0
+    while(True):
+        ans = ((2**start - 1)**2 -2)
+        if(isprime(ans) == True):
+            if(k==n):
+                return ans
+            else:
+                k = k+1
+        start = start + 1
+
     return 0
