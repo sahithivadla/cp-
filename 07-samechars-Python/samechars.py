9687ed515d49fd1c581394a22014d3ff57313731
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-25 22:57:31 +0530

    t

--- 07-samechars-Python/samechars.py
+++ 07-samechars-Python/samechars.py
@@ -1,10 +1,26 @@
 # sameChars(s1, s2) [20 pts]
-# Write the function sameChars(s1, s2) that takes two strings and returns True if the two strings are composed of 
-# the same characters (though perhaps in different numbers and in different orders) -- that is, if every character 
-# that is in the first string, is in the second, and vice versa -- and False otherwise. This test is 
-# case-sensitive, so "ABC" and "abc" do not contain the same characters. The function returns False if either 
+# Write the function sameChars(s1, s2) that takes two strings and returns True if the two strings are composed of
+# the same characters (though perhaps in different numbers and in different orders) -- that is, if every character
+# that is in the first string, is in the second, and vice versa -- and False otherwise. This test is
+# case-sensitive, so "ABC" and "abc" do not contain the same characters. The function returns False if either
 # parameter is not a string, but returns True if both strings are empty (why?).
 
 def samechars(s1, s2):
 	# Your code goes here
-	pass
+	if(isinstance(s1,int) == True or isinstance(s2,int) == True):
+		return False
+
+	short = ""
+	longs = ""
+	if(len(s1)>len(s2)):
+		short = s2
+		longs = s1
+	else:
+		longs = s2
+		short = s1
+
+	for i in short:
+		if i not in longs:
+			return False
+	return True
+
