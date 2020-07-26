6b6351d286199d24ea69c7124afae6ad9e18370a
sahithivadla <sahithivadla1@msitprogram.net>
2020-07-26 18:25:03 +0530

    t

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -13,6 +13,7 @@ def nthwithproperty309(n):
 	while(True):
 		ans = str(pow(start,5))
 		if("0" in ans and "1" in ans and "2" in ans and "3" in ans and "4" in ans and "5" in ans and "6" in ans and "7" in ans and "8" in ans and "9" in ans):
+			print(start)
 			if(k == n):
 				return start
 			else:
